cb2c2a9a7e3824c77fd450cdd28ff4a3ed481a99
Rick Broker <rbroker@cloudbees.com>
2023-09-18 10:19:36 -0400

    reformat

---
+++ labs/cloudbees-cdro-v2/support/dsl/pipeline_Base.groovy
@@ -0,0 +1,108 @@
+pipeline 'pipeline_Base', {
+  projectName = 'Workshop'
+
+  formalParameter 'ec_stagesToRun', {
+    expansionDeferred = '1'
+  }
+
+  stage 'Release Readiness', {
+    colorCode = '#289ce1'
+    pipelineName = 'pipeline_Base'
+    gate 'POST', {
+      task 'No Code Smells', {
+        gateCondition = '$[/javascript myStageRuntime.tasks[\'Get latest SonarQube scan results\'].job.getLastSonarMetrics.code_smells != null || myStageRuntime.tasks[\'Get latest SonarQube scan results\'].job.getLastSonarMetrics.code_smells < 1]'
+        gateType = 'POST'
+        subproject = 'Workshop'
+        taskType = 'CONDITIONAL'
+      }
+    }
+
+    task 'Git changelog', {
+      actualParameter = [
+        'branch': 'main',
+        'commit': '',
+        'config': '/projects/Workshop/pluginConfigurations/Git-Source-Code-Sync',
+        'depth': '',
+        'gitRepoFolder': '/tmp/demo-app',
+        'mirror': 'false',
+        'overwrite': 'true',
+        'pathspecs': '',
+        'referenceFolder': '',
+        'repoUrl': 'https://github.com/cloudbees-days/cdro-workshop-demo-app',
+        'resultPropertySheet': '/myJob/clone',
+        'shallowSubmodules': 'false',
+        'submodules': 'false',
+        'tag': '',
+      ]
+      stageSummaryParameters = '[{"label":"cloneData","name":"cloneData"}]'
+      subpluginKey = 'EC-Git'
+      subprocedure = 'Clone'
+      taskType = 'PLUGIN'
+    }
+
+    task 'Get latest SonarQube scan results', {
+      actualParameter = [
+        'config': '/projects/Workshop/pluginConfigurations/cb-demos-sonar',
+        'resultFormat': 'propertysheet',
+        'resultSonarProperty': '/myJob/getLastSonarMetrics',
+        'sonarMetricsComplexity': 'all',
+        'sonarMetricsDocumentation': 'all',
+        'sonarMetricsDuplications': 'all',
+        'sonarMetricsIssues': 'all',
+        'sonarMetricsMaintainability': 'all',
+        'sonarMetricsMetrics': 'all',
+        'sonarMetricsQualityGates': 'all',
+        'sonarMetricsReliability': 'all',
+        'sonarMetricsSecurity': 'all',
+        'sonarMetricsTests': 'all',
+        'sonarProjectKey': 'petclinic',
+        'sonarProjectName': 'petclinic',
+        'sonarProjectVersion': '2.2.0.BUILD-SNAPSHOT',
+        'sonarTaskId': '',
+        'sonarTimeout': '',
+      ]
+      subpluginKey = 'EC-SonarQube'
+      subprocedure = 'Get Last SonarQube Metrics'
+      taskType = 'PLUGIN'
+    }
+  }
+
+  stage 'Quality Assurance', {
+    colorCode = '#ff7f0e'
+    pipelineName = 'pipeline_Base'
+    task 'Deploy to QA', {
+      command = 'echo "Deploy to QA"'
+      taskType = 'COMMAND'
+    }
+  }
+
+  stage 'Production', {
+    colorCode = '#2ca02c'
+    pipelineName = 'pipeline_Base'
+    gate 'PRE', {
+      task 'Manual approval', {
+        gateType = 'PRE'
+        notificationEnabled = '1'
+        notificationTemplate = 'ec_default_gate_task_notification_template'
+        subproject = 'Workshop'
+        taskType = 'APPROVAL'
+        approver = [
+          'rbroker',
+        ]
+      }
+    }
+
+    task 'Deploy to Production', {
+      command = 'echo "Deploy to Production"'
+      taskType = 'COMMAND'
+    }
+  }
+
+  // Custom properties
+
+  property 'ec_counters', {
+
+    // Custom properties
+    pipelineCounter = '1'
+  }
+}
