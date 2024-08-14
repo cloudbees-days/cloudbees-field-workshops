c8b37aeb24b63cc6a007ebf1602808bdbdba17cd
Rick Broker <rbroker@cloudbees.com>
2024-08-14 11:59:27 -0500

    add build the world for use with cdro-cli

---
+++ labs/cloudbees-cdro-v2/support/dsl/buildTheWorld.groovy
@@ -0,0 +1,5 @@
+@@includeFile workshopGroup.groovy@@
+@@includeFile createUser.groovy@@
+@@includeFile deleteUser.groovy@@
+@@includeFile pipeliine_Base.groovy@@
+@@includeFile Workshop_Service_Catalog.groovy@@
